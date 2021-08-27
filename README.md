10df925cffce9f55175a378fc181e147a3092cca
Sandro Habicher <sandro.habicher@gmail.com>
2021-08-27 22:57:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Node Wireguard
+
+This is a placeholder module for a Node.JS wrapper for Wireguard.
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { ListScan } from './scans/list-scan/ListScan'
+import { ListScanOption } from './scans/list-scan/ListScanOption'
+import { NmapScan } from './scans/nmap-scan/NmapScan'
+import { NmapScanOption } from './scans/nmap-scan/NmapScanOption'
+import NmapScanResponse from './scans/nmap-scan/NmapScanResponse'
+
+export {
+    NmapScan,
+    NmapScanOption,
+    NmapScanResponse,
+    ListScan,
+    ListScanOption
+}
